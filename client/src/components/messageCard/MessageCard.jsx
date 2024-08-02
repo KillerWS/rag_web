8e0f04cac14b42644865b9582642a9cfedac3f77
Chris <495032732@qq.com>
2024-08-02 13:56:42 +0800

    x

--- client/src/components/messageCard/MessageCard.jsx
+++ client/src/components/messageCard/MessageCard.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
-
-
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Avatar, Card } from 'antd';
+import { Button, Modal } from 'antd';
 
 const MessageCard = ({message, fromUser}) => {
-    
+    const { Meta } = Card;
+
     const [msg, setMsg] = useState({text:"", isFromUser:true});
 
     useEffect(()=>{
@@ -24,11 +26,26 @@ const MessageCard = ({message, fromUser}) => {
         return (
             <div className="flex flex-col space-y-2">
          <div className="flex items-center justify-end gap-1">
-         <div className="bg-blue-500 text-white text-base rounded-lg p-2 max-w-xs">{msg.text}</div>
+         <div className="bg-blue-500 text-white text-base rounded-lg p-2 max-w-xs">
+            {msg.text}
+         </div>
         <img className="w-8 h-8 rounded-full mr-2" src="https://via.placeholder.com/50" alt="User" />
-        
+            
         </div>
         </div>  
+        // <div className="flex flex-col space-y-2">
+
+
+
+        //     <div className="flex items-center justify-end gap-1"> 
+        //                 <div className="w-72 p-0">
+        //         <div className="text-black text-base rounded-lg break-words whitespace-pre-wrap ">
+        //             {msg.text}
+        //         </div>
+        //         <img className="w-8 h-8 rounded-full mr-2" src="https://via.placeholder.com/50" alt="User" />
+        //         </div>
+        //         </div>
+        //     </div>
     
         )
     }else{
@@ -36,8 +53,16 @@ const MessageCard = ({message, fromUser}) => {
             <div className="flex flex-col space-y-2">
                 <div className="flex items-center gap-1">
                 <img className="w-8 h-8 rounded-full ml-2" src="https://via.placeholder.com/50" alt="User" />
-                <div className="bg-gray-300 text-gray-800 rounded-lg p-2 max-w-xs">{msg.text}</div>
-                
+                {/* <div className="bg-gray-300 text-gray-800 rounded-lg p-2 max-w-xs">{msg.text}</div> */}
+                <Card className="w-72 p-0"
+                actions={[
+                    <SettingOutlined key="setting" />,
+                    <EditOutlined key="edit" />,
+                  ]}>
+                <div className="text-black text-base rounded-lg break-words whitespace-pre-wrap ">
+                    {msg.text}
+                </div>
+                </Card>
             </div>
             </div>
         )
